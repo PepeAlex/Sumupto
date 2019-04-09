307ac4a1fab1feab056e373c0651c85ac0efe191
Alexander Pepe <G00376284@gmit.ie>
2019-04-09 16:05:45 +0100

    first commit

---
+++ sumupto.py
@@ -0,0 +1,12 @@
+# Alexander Pepe
+# Problem Set 2019
+# Question 1
+
+a = int(input("Tell me a positive numbers: "))
+
+x = sum(range(0, (a + 1)))
+
+print("""
+The sum of the numbers is %d
+Thank you
+""" % x)    
